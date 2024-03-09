4b1342fb024e1d57ad398f288eb4c30a28574c0c
Dima <javascripter27@gmail.com>
2024-03-09 16:39:28 +0300

    Final version

--- main.py
+++ main.py
@@ -78,16 +78,22 @@ def where_line_crosses_circle(R, angle, num, F, delta):
     x2 = (-2 * k * b - sqrt(D)) / (2 * (k ** 2 + 1))
     return x1, x2
 
+def rotate_coord(x, y, share):
+    share = radians(share)
+    x_new = x*cos(share) + y*sin(share)
+    y_new = -sin(share)*x + cos(share)*y
+    return x_new, y_new
 
-def sector(angle, R, F, delta):
+
+def sector(angle, R, F, delta, share):
     """Function that forms a filled sector!"""
     # x_edge and y_edge are points of intersection sector's line (not for x = 0!) and circle
     x_edge = R * sin(radians(angle))
     y_edge = - R * cos(radians(angle))
     # first actions are actions to create a circle a sector without filling
-    first_actions = '{0}\n{1}\n{2}\n{3}\n'.format(abs_vector(0, -R),
+    first_actions = '{0}\n{1}\n{2}\n{3}\n'.format(abs_vector(*rotate_coord(0, -R, share)),
                                                   abs_visible_vector(0, 0),
-                                                  abs_visible_vector(x_edge, y_edge),
+                                                  abs_visible_vector(*rotate_coord(x_edge, y_edge, share)),
                                                   abs_vector(0, 0)
                                                   )
     # loop_actions consist of actions for filling a sector
@@ -109,15 +115,17 @@ def sector(angle, R, F, delta):
             if func(0, angle, k, F, delta) < -R:
                 x = x1
                 # Note a point on circle
-                loop_actions.append(abs_vector(x, -sqrt(R ** 2 - x ** 2)))
+                loop_actions.append(abs_vector(*rotate_coord(x, -sqrt(R ** 2 - x ** 2), share)))
                 # Note a point on sector's line
-                loop_actions.append(abs_visible_vector(*crossed_point_r(angle, k, F, delta)))
+                args = [elem for elem in crossed_point_r(angle, k, F, delta)] + [share]
+                loop_actions.append(abs_visible_vector(*rotate_coord(*args)))
                 k += 1
                 continue
             # If there is not a line that connects circle and sector's line, It will a line
             # that connects a line x = 0 and sector's line:
-            loop_actions.append(abs_vector(0, func(0, angle, k, F, delta)))
-            loop_actions.append(abs_visible_vector(*crossed_point_r(angle, k, F, delta)))
+            loop_actions.append(abs_vector(*rotate_coord(0, func(0, angle, k, F, delta), share)))
+            args = [elem for elem in crossed_point_r(angle, k, F, delta)] + [share]
+            loop_actions.append(abs_visible_vector(*rotate_coord(*args)))
             k += 1
 
         # There is an algorithm to connect two points on the circle, if they exist considering actual conditions
@@ -129,16 +137,16 @@ def sector(angle, R, F, delta):
                 x2 = x1
                 x1 = x2_
             if 0 < x1 < x2 < x_edge:
-                loop_actions.append(abs_vector(x1, -sqrt(R ** 2 - x1 ** 2)))
-                loop_actions.append(abs_visible_vector(x2, -sqrt(R ** 2 - x2 ** 2)))
+                loop_actions.append(abs_vector(*rotate_coord(x1, -sqrt(R ** 2 - x1 ** 2), share)))
+                loop_actions.append(abs_visible_vector(*rotate_coord(x2, -sqrt(R ** 2 - x2 ** 2), share)))
                 k += 1
     else:
         # It's an algorithm for acute angle.
         while b_coef(angle, k, F, delta) >= -R:
             x, y = crossed_point_r(angle, k, F, delta)
             if x <= x_edge:
-                loop_actions.append(abs_vector(0, b_coef(angle, k, F, delta)))
-                loop_actions.append(abs_visible_vector(x, y))
+                loop_actions.append(abs_vector(*rotate_coord(0, b_coef(angle, k, F, delta), share)))
+                loop_actions.append(abs_visible_vector(*rotate_coord(x, y, share)))
             x1, x2 = where_line_crosses_circle(R, angle, k, F, delta)
             if x1 > 0:
                 x = x1
@@ -148,18 +156,38 @@ def sector(angle, R, F, delta):
                 x = min((x1, x2))
                 print(x)
             if x <= x_edge:
-                loop_actions.append(abs_vector(x, -sqrt(R ** 2 - x ** 2)))
-                loop_actions.append(abs_visible_vector(0, b_coef(angle, k, F, delta)))
+                loop_actions.append(abs_vector(*rotate_coord(x, -sqrt(R ** 2 - x ** 2), share)))
+                loop_actions.append(abs_visible_vector(*rotate_coord(0, b_coef(angle, k, F, delta), share)))
             k += 1
     return first_actions + ''.join(loop_actions)
 
-def main(angle, R, F, delta, file_name='minimarker_script.lsc'):
+def main(R, A, B, H, F, N, file_name='minimarker_script.lsc'):
     with open(file_name, 'w') as file:
+
+        share = 360/N
+        share_f = (B - A)/N
+        share_p = (100-10)/N
+
         file.write(to_decart())
         file.write(circle(R))
-        file.write(sector(angle, R, F, delta))
+        filling_step = A
+        power_step = 10
+        for i in range(N):
+            file.write(power(power_step))
+            if power_step + share_p < 100:
+                power_step += share_p
+            file.write(sector(share, R, F, filling_step, i*share))
+            filling_step += share_f
 
-main(10, 25, 130, 5)
+
+R = float(input('Радиус R: '))
+A = float(input('Нижняя граница мощности заливки A: '))
+B = float(input('Верхняя граница мощности заливки B: '))
+H = float(input('Шаг роста мощности заливки H: '))
+F = float(input('Угол заливки относительно биссектрисы (в градусах) F: '))
+N = int(input('Число секторов N: '))
+
+main(R, A, B, H, F, N)
 
 
 
